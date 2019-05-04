e6ef415f59c0d2187f96e45ff7032e054694716e
Maxim Pogrebnoy <maxlanser@gmail.com>
2019-05-04 23:54:23 +0300

    bootstrap-grid

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,16 +1,30 @@
-<div class="container">
-	<ul class="nav nav-pills">
-	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="/">Home</a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="/about">About Us</a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="/contacts">Contact</a>
-	  </li>
-		<li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Products") {?>active<?php }?>" href="/products">Products</a>
-	  </li>
-	</ul>
+<div class="header-bg">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col-lg-3">
+				<a href="" class="logo">
+					<img src="images/logo.png" alt="">
+				</a>
+			</div>
+			<div class="col-lg-7">
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="/">Главная</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">История</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">О компании</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Продукция</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Контакты</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
