a88d0c2c1ec57720271a847c3008221ec3d235cf
DarchukSergey <DarchukSergey@gmail.com>
2019-05-15 16:52:57 +0300

    nav arrow

--- includes/navigation.php
+++ includes/navigation.php
@@ -9,7 +9,7 @@
          <div class="col-12 col-sm-8 col-md-9 col-lg-8 main-menu">
             <nav class="navbar navbar-expand-md justify-content-end">
                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown">
-               <span class="navbar-toggler-icon">Меню</span>
+               <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i>Меню</span>
                </button>
                <div class="collapse navbar-collapse" id="navbarNavDropdown">
                   <ul class="navbar-nav">
@@ -20,13 +20,14 @@
                         <a class="nav-link <?php if ($CURRENT_PAGE == "history") {?>active<?php }?>" href="/history/">История</a>
                      </li>
                      <li class="nav-item dropdown">
-                        <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="#">О компании</a>
+                        <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="#">О компании<i class="fas fa-angle-down"></i></i></a>
+                        
                         <ul class="dropdown-menu dropdown-menu-left">
                            <li><a class="dropdown-item" href="/about/winerys/">Производства</a></li>
                            <li><a class="dropdown-item" href="/about/shops/">Розничная сеть</a></li>
                            <li><a class="dropdown-item" href="/about/museum/">Музей виноделия</a></li>
                            <li class="dropdown-submenu">
-                              <a class="dropdown-item" href="#">Акционерам</a>
+                              <a class="dropdown-item" href="#">Акционерам<i class="fas fa-angle-down"></i></a>
                               <ul class="dropdown-menu">
                                  <li><a class="dropdown-item" href="/about/stockholders/produce/">АО «Прасковейское»</a></li>
                                  <li><a class="dropdown-item" href="/about/stockholders/trade/">АО «ТП Прасковейское»</a></li>
@@ -35,10 +36,10 @@
                         </ul>
                      </li>
                      <li class="nav-item dropdown">
-                        <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="#">Продукция</a>
+                        <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="#">Продукция<i class="fas fa-angle-down"></i> </a>
                         <ul class="dropdown-menu dropdown-menu-left">
                            <li  class="dropdown-submenu">
-                              <a class="dropdown-item" href="#">Вина</a>
+                              <a class="dropdown-item" href="#">Вина<i class="fas fa-angle-down"></i></a>
                               <ul class="dropdown-menu">
                                  <li><a class="dropdown-item" href="/products/wine/blanc/">Белые</a></li>
                                  <li><a class="dropdown-item" href="/products/wine/rose/">Розовые</a></li>
@@ -48,7 +49,7 @@
                               </ul>
                            </li>
                            <li class="dropdown-submenu">
-                              <a class="dropdown-item" href="#">Коньяки</a>
+                              <a class="dropdown-item" href="#">Коньяки<i class="fas fa-angle-down"></i></a>
                               <ul class="dropdown-menu">
                                  <li><a class="dropdown-item" href="/products/cognac/ordin/">Ординарные</a></li>
                                  <li><a class="dropdown-item" href="/products/cognac/mrn/">Марочные</a></li>
@@ -58,7 +59,7 @@
                         </ul>
                      </li>
                      <li class="nav-item dropdown">
-                        <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="#">Контакты</a>
+                        <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="#">Контакты<i class="fas fa-angle-down"></i> </a>
                         <div class="dropdown-menu dropdown-menu-right">
                            <a class="dropdown-item" href="/contacts/">Адрес</a>
                            <a class="dropdown-item" href="/feedback/">Обратная связь</a>
